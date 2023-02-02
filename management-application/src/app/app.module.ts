d6d755c020aecbe9fd4f7ee5a47b0773b213d67c
Kanchan <kanchandeep.kaur@lpinfotech.com>
2023-02-02 11:17:22 +0530

    Updated folder structure and created new components.

--- management-application/src/app/app.module.ts
+++ management-application/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 import { ForgotPasswrdComponent } from './account/forgot-passwrd/forgot-passwrd.component';
-import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
-import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard/admin-dashboard.component';
-import { EditRecordComponent } from './admin/edit-record/edit-record/edit-record.component';
-import { AddRecordComponent } from './admin/add-record/add-record/add-record.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { EditRecordComponent } from './admin//edit-record/edit-record.component';
+import { AddRecordComponent } from './admin/add-record/add-record.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AsideNavComponent } from './shared/aside/aside-nav.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +19,13 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
     LoginComponent,
     RegisterComponent,
     ForgotPasswrdComponent,
-    UserDashboardComponent,
     AdminDashboardComponent,
     EditRecordComponent,
     AddRecordComponent,
-    DashboardComponent
+    DashboardComponent,
+    AsideNavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- management-application/src/app/shared/header/header.component.spec.ts
+++ management-application/src/app/shared/header/header.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserDashboardComponent } from './user-dashboard.component';
+import { HeaderComponent } from './header.component';
 
-describe('UserDashboardComponent', () => {
-  let component: UserDashboardComponent;
-  let fixture: ComponentFixture<UserDashboardComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserDashboardComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(UserDashboardComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
